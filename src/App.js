9ccd36fcdc9571d4ee4397ab926a01761e4ee4c4
0Sakshisingh <sakshisng27@gmail.com>
2024-09-23 00:37:34 +0530

    Movie_IMDB_Clone Finshed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './component/Header.js'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './page/home/Home.js';
+import MovieList from './component/MovieList.js';
+import MovieDetail from './component/MovieDetail.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+         <Header/>
+        <Routes>
+          <Route index element={<Home/>}></Route>
+          <Route path="movie/:id" element={<MovieDetail/>}></Route> 
+          <Route path="movies/:type" element={<MovieList/>}></Route>
+          <Route path="/*"element={<h1>error page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
